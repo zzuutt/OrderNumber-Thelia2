010b3a770bba6c850b68f2103f4499a84403c51d
zzuutt <zzuutt@users.noreply.github.com>
2014-07-01 11:57:56 +0200

    Create fr_FR.php

---
+++ I18n/fr_FR.php
@@ -0,0 +1,9 @@
+<?php
+
+return array(
+    'Can\'t read file ' => 'impossible de lire le fichier',
+    'Can\'t write file ' => 'impossible d\'ecrire le fichier',
+    'PersonalValue' => 'Valeur personnelle',
+    'The file ' => 'le fichier',
+    'The file is corrupted.' => 'Le fichier est corrompu',
+);
