e78a5066873749cdaff292c185bf4d83df66f176
zzuutt <zzuutt@users.noreply.github.com>
2014-07-01 12:00:31 +0200

    Create ConfigInterface.php

---
+++ Model/ConfigInterface.php
@@ -0,0 +1,17 @@
+<?php
+namespace OrderNumber\Model;
+
+interface ConfigInterface
+{
+    // Data access
+    public function write($file=null);
+    public static function read($file=null);
+
+    // variables setters
+
+    /*
+     * @return OrderNumber\Model\ConfigInterface
+     */
+    public function setORDERNUMBERPERSONALVALUE($ORDERNUMBER_PERSONALVALUE);
+
+}
