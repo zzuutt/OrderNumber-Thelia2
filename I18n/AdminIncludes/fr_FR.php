e8f19136b55a497363b587631c783414cd6a440f
zzuutt <zzuutt@users.noreply.github.com>
2014-07-01 11:57:03 +0200

    Create fr_FR.php

---
+++ I18n/AdminIncludes/fr_FR.php
@@ -0,0 +1,13 @@
+<?php
+
+return array(
+    'Commande disponible :' => 'Commande disponible : ',
+    'Customizing your invoice number' => 'Personnalisation de votre numéro de facture ',
+    'Exemple avec un id=99:' => 'Exemple avec un id=99: ',
+    'Order number setup' => 'Paramètre Numero de facture',
+    'PersonalValue' => 'Valeur personnelle',
+    'Please change the access rights' => 'Svp changez les droit d\'acces',
+    'Save' => 'Enregistrer',
+    'Your personal value' => 'Votre valeur personnelle',
+    'va donner' => 'va donner ',
+);
