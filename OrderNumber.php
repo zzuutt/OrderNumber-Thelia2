21249bf088c5550a103398e6c4f9d138ad41d91f
zzuutt <zzuutt@users.noreply.github.com>
2014-07-01 11:45:31 +0200

    Create OrderNumber.php

---
+++ OrderNumber.php
@@ -0,0 +1,22 @@
+<?php
+/*************************************************************************************/
+/*                                                                                   */
+/*                                                                                   */
+/*      email : zzuutt34@free.fr                                                     */
+/*                                                                                   */
+/*************************************************************************************/
+
+namespace OrderNumber;
+use Cbatos\Model\Config;
+use Thelia\Module\BaseModule;
+use Thelia\Core\Translation\Translator;
+
+class OrderNumber extends BaseModule
+{
+    const JSON_CONFIG_PATH = "/Config/config.json";
+
+    public function getCode()
+    {
+        return 'OrderNumber';
+    }
+}
