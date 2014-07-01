98323bd9cb66f71378e0977de246668df56a13d5
zzuutt <zzuutt@users.noreply.github.com>
2014-07-01 11:57:24 +0200

    Create en_US.php

---
+++ I18n/AdminIncludes/en_US.php
@@ -0,0 +1,13 @@
+<?php
+
+return array(
+    'Commande disponible :' => 'Order available',
+    'Customizing your invoice number' => 'Customizing your invoice number ',
+    'Exemple avec un id=99:' => 'Example with id=99:',
+    'Order number setup' => 'Order number setup ',
+    'PersonalValue' => 'Personal value',
+    'Please change the access rights' => 'Please change the access rights ',
+    'Save' => 'Save',
+    'Your personal value' => 'Your personal value ',
+    'va donner' => 'will give',
+);
