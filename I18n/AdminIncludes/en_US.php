fe2df4aa3a70feb9830852a6e38046d88e057593
zzuutt <zzuutt@users.noreply.github.com>
2014-09-22 09:04:30 +0200

    Update en_US.php

--- I18n/AdminIncludes/en_US.php
+++ I18n/AdminIncludes/en_US.php
@@ -2,7 +2,7 @@
 
 return array(
     'Commande disponible :' => 'Order available',
-    'Customizing your invoice number' => 'Customizing your invoice number ',
+    'Customizing your invoice number' => 'Customizing your order / invoice number ',
     'Exemple avec un id=99:' => 'Example with id=99:',
     'Order number setup' => 'Order number setup ',
     'PersonalValue' => 'Personal value',
