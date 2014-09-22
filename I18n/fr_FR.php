6129825a1f9ed288fe994e27d13254f2f71ebd33
zzuutt <zzuutt@users.noreply.github.com>
2014-09-22 09:01:44 +0200

    Update fr_FR.php

--- I18n/fr_FR.php
+++ I18n/fr_FR.php
@@ -3,7 +3,8 @@
 return array(
     'Can\'t read file ' => 'impossible de lire le fichier',
     'Can\'t write file ' => 'impossible d\'ecrire le fichier',
-    'PersonalValue' => 'Valeur personnelle',
+    'InvoicePersonalValue' => 'Masque pour le numéro de facture',
+    'PersonalValue' => 'Masque pour le Numéro de commande',
     'The file ' => 'le fichier',
     'The file is corrupted.' => 'Le fichier est corrompu',
 );
